1017fa94ae8b16faa823c74f787a9f14a03f918b
HyperMonkeyTERR <131418063+HyperMonkeyTERR@users.noreply.github.com>
2023-04-20 23:26:29 -0400

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"644201c54b38c40020050b35",
+				"644201c54b38c40020050b38"
+			]
+		},
+		"644201c54b38c40020050b35": {
+			"id": "644201c54b38c40020050b35",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"644201c54b38c40020050b38": {
+			"id": "644201c54b38c40020050b38",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
